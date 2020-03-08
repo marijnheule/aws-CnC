5026a138670ee25a41e3f95313fe34fa32af1c0f
Heule <mheule@a483e71b7bd5.ant.amazon.com>
2020-03-08 12:34:40 -0400

    partitioning in parallel

--- par-cube-glucose.sh
+++ par-cube-glucose.sh
@@ -151,30 +151,29 @@ do
   /CnC/scripts/apply.sh $CNF /CnC/cubes-split-${AWS_BATCH_JOB_NODE_INDEX}.txt $CORE > $OUT/node-$CORE.cnf
   $DIR/cadical/build/cadical $OUT/node-$CORE.cnf -c 100000 -o $OUT/simp-$CORE.cnf -q > $OUT/simp-result-$CORE.txt &
   PIDS[$CORE]=$!
-  echo "c simplifying subformula "$CORE" on process "$!
+  log "simplifying subformula "$CORE" on process "$!
 done
 
 # wait for all pids
 for (( CORE=1; CORE<=$MIN; CORE++ )) do wait ${PIDS[$CORE]}; done
 
-##### partition all simplied subformulas ######
-
-# still sequential, must be parallelized
+##### partition all simplied subformulas in parallel ######
+JOBS=0
 for (( CORE=1; CORE<=$MIN; CORE++ ))
 do
   echo "a 0" > $OUT/cubes-$CORE.txt
   echo -n "c subformula result on core "$CORE": "; cat $OUT/simp-result-$CORE.txt
   RES=`cat $OUT/simp-result-$CORE.txt | grep -e "SATIS" -e "UNKNOWN" | awk '{print $2}'`
   if [ "$RES" == "$UNK" ]; then
-    $DIR/march_cu/march_cu $OUT/simp-$CORE.cnf -o $OUT/cubes-$CORE.txt -d $DEPTH
-#    /CnC/scripts/prefix.sh /CnC/cubes-split-${AWS_BATCH_JOB_NODE_INDEX}.txt $CORE $OUT/cubes-$CORE.txt >> $OUT/cubes-merge-$$.txt
-#  else
-#    log "simpified subformula solved"
-#    head -n $CORE /CnC/cubes-split-${AWS_BATCH_JOB_NODE_INDEX}.txt | tail -n 1 >> $OUT/cubes-merge-$$.txt
+    JOBS=$(($JOBS + 1))
+    $DIR/march_cu/march_cu $OUT/simp-$CORE.cnf -o $OUT/cubes-$CORE.txt -d $DEPTH &
+    PIDS[$JOBS]=$!
+    log "partitioning simplified subformula "$CORE" on process "$!
   fi
-#  rm $OUT/node-$CORE.cnf $OUT/simp-result-$CORE.txt $OUT/simp-$CORE.cnf
 done
 
+# wait for all pids
+for (( CORE=1; CORE<=$JOBS; CORE++ )) do wait ${PIDS[$CORE]}; done
 
 ##### merge all cubes and clean up #####
 
