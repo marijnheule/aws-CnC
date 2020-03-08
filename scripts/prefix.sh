eb93428712620b5e197ad3edf813225e73eff76c
Heule <mheule@a483e71b7bd5.ant.amazon.com>
2020-03-07 22:27:37 -0500

    tail

--- scripts/prefix.sh
+++ scripts/prefix.sh
@@ -3,7 +3,7 @@ LINE=$2
 NEWC=$3
 
 IFS=$'\n'
-PREFIX=`head -n $LINE $OLDC | sed 's| 0||'`
+PREFIX=`head -n $LINE $OLDC | tail -n 1 | sed 's| 0||'`
 for CUBE in `cat $NEWC | sed 's|a||'`
 do
   echo -n $PREFIX
